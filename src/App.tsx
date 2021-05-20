56063e5ca060e9c4074d1ecce5cdb9f34428ef90
Tony Aguilera <toni.aguilera@cabsa.es>
2021-05-20 15:27:13 +0200

    Developing Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,11 @@
-import React from 'react';
 import './App.css';
-
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import { Users } from './screens/Users';
-import { Wallets } from './screens/Wallets';
 import { Home } from './screens/Home';
-
 function App() {
   return (
-    <Router>
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/users">Users</Link>
-        </li>
-        <li>
-          <Link to="/wallets">Wallets</Link>
-        </li>
-      </ul>
+  
+    <><Home/></>
 
-      <Switch>
-        <Route path="/users">
-          <Users />
-        </Route>
-        <Route path="/wallets">
-          <Wallets />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </div>
-  </Router>
+      
   );
 }
 
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
 
 export const Home = () => {
     return (
         <>
-            <h3>Home Page</h3>
+            <h3 className="d-flex justify-content-center mt-4">Welcome to Wallets!</h3>
+
+            <div className="d-flex justify-content-left">
+                <p>This is the menu</p>
+            </div>
         </>
     )
-}
+};
