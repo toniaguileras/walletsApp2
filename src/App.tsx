9a49101f48878c0192d8701fcd967cb865c0c462
Tony Aguilera <toni.aguilera@cabsa.es>
2021-05-20 11:48:06 +0200

    First commit app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { Users } from './screens/Users';
+import { Wallets } from './screens/Wallets';
+import { Home } from './screens/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/users">Users</Link>
+        </li>
+        <li>
+          <Link to="/wallets">Wallets</Link>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/wallets">
+          <Wallets />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
+  </Router>
   );
 }
 
---
+++ src/screens/Wallets.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Wallets = () => {
+    return (
+        <>
+            <h3>Wallets Page</h3>
+        </>
+    )
+}
